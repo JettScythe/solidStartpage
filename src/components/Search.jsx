f2c623b74e4423ece48ab71d14b50b640bfe6f0f
JettScythe <jettscythe@hotmail.com>
2022-07-19 22:50:43 -0300

    feat(search & devdeps): opens searcgh in new tab, adds husky / pre-commit / etc
    adds search functionality with a variety of search providers

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,20 +6,35 @@ import {
   InputGroup,
   InputRightElement,
 } from "@hope-ui/solid";
+import { formatSearchProviderURL } from "../helpers/formatters";
+import { useSearchProvider } from "../helpers/SearchProvider";
 import { IconSearch } from "../icons/IconSearch";
+import { createSignal } from "solid-js";
+
+const openTabSearch = (searchProvider, searchTerm) => {
+  const searchUrl = formatSearchProviderURL(searchProvider);
+  const url = `${searchUrl}${searchTerm}`;
+  window.open(url, "_blank");
+};
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = createSignal("");
+  const [state, { setSearchProvider }] = useSearchProvider();
+  const handleInput = (event) => {
+    setSearchTerm(event.target.value);
+  };
   return (
     <FormControl>
       <FormLabel for="search">Search</FormLabel>
       <InputGroup>
-        <Input id="search" type="search" />
+        <Input id="search" type="search" onChange={handleInput} />
         <InputRightElement>
           <IconButton
             variant="outline"
             aria-label="Search"
             icon={<IconSearch />}
             type="submit"
+            onClick={() => openTabSearch(state(), searchTerm())}
           />
         </InputRightElement>
       </InputGroup>
