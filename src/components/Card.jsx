4121dfadc28e8693d96d42828786de03b204b5b7
JettScythe <jettscythe@hotmail.com>
2022-07-22 10:04:14 -0300

    feat(card): formatted document, updated links

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,62 +1,142 @@
-import { Anchor, Divider } from "@hope-ui/solid"
-import '../css/Card.css'
-
+import { Anchor, Divider } from "@hope-ui/solid";
+import "../css/Card.css";
 
 const Card = () => {
   return (
-    <div class="row">
-      <div class="column">
-        <div class="card">
-            <p>
-                Social
-            </p>
-            <Divider color={"$primary10"}/>
-            <Anchor href='https://twitter.com' class="row" external>Twitter</Anchor>
-            <Anchor href='https://noise.cash' class="row" external>Noise</Anchor>
-            <Anchor href='https://memo.cash' class="row" external>Memo</Anchor>
-            <Anchor href='https://member.cash' class="row" external>Member</Anchor>
+    <>
+      <div class="row">
+        <div class="column">
+          <div class="card">
+            <p>Social</p>
+            <Divider color={"$primary10"} />
+            <Anchor href="https://twitter.com" class="row" external>
+              Twitter
+            </Anchor>
+            <Anchor href="https://noise.cash" class="row" external>
+              Noise
+            </Anchor>
+            <Anchor href="https://memo.cash" class="row" external>
+              Memo
+            </Anchor>
+            <Anchor href="https://member.cash" class="row" external>
+              Member
+            </Anchor>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <p>Dev</p>
+            <Divider color={"$accent10"} />
+            <Anchor href="https://code.jettscythe.xyz" class="row" external>
+              Code Cloud
+            </Anchor>
+            <Anchor href="https://github.com" class="row" external>
+              Github
+            </Anchor>
+            <Anchor href="https://gitlab.com" class="row" external>
+              Gitlab
+            </Anchor>
+            <Anchor href="https://gist.github.com" class="row" external>
+              Gist
+            </Anchor>
+            <Anchor
+              href="https://web.compass.lighthouselabs.ca/"
+              class="row"
+              external
+            >
+              Compass
+            </Anchor>
+            <Anchor href="https://udemy.com" class="row" external>
+              Udemy
+            </Anchor>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <p>Media</p>
+            <Divider color={"$warning10"} />
+            <Anchor
+              href="https://torrents.jettscythe.local"
+              class="row"
+              external
+            >
+              Torrents
+            </Anchor>
+            <Anchor href="https://jellyfin.jettscythe.xyz" class="row" external>
+              Jellyfin
+            </Anchor>
+            <Anchor href="https://youtube.com" class="row" external>
+              Youtube
+            </Anchor>
+            <Anchor href="https://seerr.jettscythe.local" class="row" external>
+              Seerr
+            </Anchor>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <p>Local</p>
+            <Divider color={"$success10"} />
+            <Anchor
+              href="https://explorer.jettscythe.local"
+              class="row"
+              external
+            >
+              BCH explorer
+            </Anchor>
+            <Anchor
+              href="https://address.jettscythe.local"
+              class="row"
+              external
+            >
+              addr explorer
+            </Anchor>
+            <Anchor href="https://sonarr.jettscythe.local" class="row" external>
+              sonarr
+            </Anchor>
+            <Anchor href="https://radarr.jettscythe.local" class="row" external>
+              radarr
+            </Anchor>
+            <Anchor href="https://lidarr.jettscythe.local" class="row" external>
+              lidarr
+            </Anchor>
+            <Anchor
+              href="https://prowlarr.jettscythe.local"
+              class="row"
+              external
+            >
+              prowlarr
+            </Anchor>
+          </div>
         </div>
       </div>
-      <div class="column">
-        <div class="card">
-            <p>
-                Dev
-            </p>
-            <Divider color={"$accent10"}/>
-            <Anchor href='https://github.com' class="row" external>Github</Anchor>
-            <Anchor href='https://gitlab.com' class="row" external>Gitlab</Anchor>
-            <Anchor href='https://gist.github.com' class="row" external>Gist</Anchor>
-            <Anchor href='https://web.compass.lighthouselabs.ca/' class="row" external>Compass</Anchor>
-            <Anchor href='https://udemy.com' class="row" external>Udemy</Anchor>
+      <div class="row">
+        <div class="column">
+          <div class="card">
+            <p>Admin</p>
+            <Divider color={"$danger10"} />
+            <Anchor href="https://pihole.jettscythe.local/admin" class="row" external>
+              Pi-hole (main)
+            </Anchor>
+            <Anchor href="https://proxmox.jettscythe.local" class="row" external>
+              Proxmox
+            </Anchor>
+            <Anchor href="https://asus.jettscythe.local" class="row" external>
+              Asus
+            </Anchor>
+            <Anchor href="https://microtik.jettscythe.local" class="row" external>
+              Mikrotik
+            </Anchor>
+            <Anchor href="https://idrac.jettscythe.local" class="row" external>
+              IDRAC
+            </Anchor>
+            <Anchor href="https://kuma.jettscythe.local" class="row" external>
+              Kuma
+            </Anchor>
+          </div>
         </div>
       </div>
-      <div class="column">
-        <div class="card">
-            <p>
-                Media
-            </p>
-            <Divider color={"$warning10"}/>
-            <Anchor href='https://torrents.jettscythe.local' class="row" external>Torrents</Anchor>
-            <Anchor href='https://jellyfin.jettscythe.xyz' class="row" external>Jellyfin</Anchor>
-            <Anchor href='https://youtube.com' class="row" external>Youtube</Anchor>
-            <Anchor href='https://seerr.jettscythe.local' class="row" external>Seerr</Anchor>
-        </div>
-      </div>
-      <div class="column">
-        <div class="card">
-            <p>
-                Local
-            </p>
-            <Divider color={"$success10"}/>
-              <Anchor href='https://explorer.jettscythe.local' class="row" external>BCH explorer</Anchor>
-              <Anchor href='https://address.jettscythe.local' class="row" external>addr explorer</Anchor>
-              <Anchor href='https://sonarr.jettscythe.local' class="row" external>sonarr</Anchor>
-              <Anchor href='https://radarr.jettscythe.local' class="row" external>radarr</Anchor>
-              <Anchor href='https://lidarr.jettscythe.local' class="row" external>lidarr</Anchor>
-              <Anchor href='https://prowlarr.jettscythe.local' class="row" external>prowlarr</Anchor>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
