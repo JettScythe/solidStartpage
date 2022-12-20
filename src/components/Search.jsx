fd7712cb5f51f088439817f646e082ac1e3f1c5c
JettScythe <jettscythe@hotmail.com>
2022-12-19 20:19:50 -0400

    feat(Search.jsx) search on enter

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -11,34 +11,43 @@ import { useSearchProvider } from "../helpers/SearchProvider";
 import { IconSearch } from "../icons/IconSearch";
 import { createSignal } from "solid-js";
 
-const openTabSearch = (searchProvider, searchTerm) => {
-  const searchUrl = formatSearchProviderURL(searchProvider);
-  const url = `${searchUrl}${searchTerm}`;
-  window.open(url, "_blank");
-};
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = createSignal("");
   const [state, { setSearchProvider }] = useSearchProvider();
+
+  const openTabSearch = () => {
+    const searchUrl = formatSearchProviderURL(state());
+    const url = `${searchUrl}${(searchTerm())}`;
+    window.open(url, "_blank");
+  };
+
   const handleInput = (event) => {
     setSearchTerm(event.target.value);
   };
+  
   return (
-    <FormControl>
-      <FormLabel for="search" color="#fff">Search</FormLabel>
+    <form onSubmit={openTabSearch}>
+      <FormLabel for="search" color="#fff">
+        Search
+      </FormLabel>
       <InputGroup>
-        <Input id="search" type="search" bgColor="$card" onChange={handleInput} />
+        <Input
+          id="search"
+          type="search"
+          bgColor="$card"
+          onChange={handleInput}
+        />
         <InputRightElement>
           <IconButton
             variant="outline"
             aria-label="Search"
             icon={<IconSearch />}
             type="submit"
-            onClick={() => openTabSearch(state(), searchTerm())}
           />
         </InputRightElement>
       </InputGroup>
-    </FormControl>
+    </form>
   );
 };
 
