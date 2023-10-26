9ec2a92dd6945497f9820bc71d2590f2fd089ea8
Jett Scythe <jettscythe@hotmail.com>
2023-10-26 00:51:39 -0300

    add icon and link to route to fix build

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,50 @@
 import {
-  Container,
-  IconButton,
-  useColorMode,
-  useColorModeValue,
-  Spacer,
-  Flex
+    Container,
+    IconButton,
+    useColorMode,
+    useColorModeValue,
+    Spacer,
+    Flex
 } from "@hope-ui/solid";
-import { IconMoon } from "../icons/IconMoon";
-import { IconSun } from "../icons/IconSun";
-import { CenteredModal } from "./Modal";
+import {IconMoon} from "../icons/IconMoon";
+import {IconSun} from "../icons/IconSun";
+import {IconBCH} from "../icons/IconBCH";
+import {CenteredModal} from "./Modal";
+import {A} from "@solidjs/router";
 
 const Header = () => {
-  const { toggleColorMode } = useColorMode();
-  const colorModeButtonIcon = useColorModeValue(<IconMoon />, <IconSun />);
+    const {toggleColorMode} = useColorMode();
+    const colorModeButtonIcon = useColorModeValue(<IconMoon/>, <IconSun/>);
 
-  return (
-    <Container h="$full">
-      <Flex>
-      <Spacer />
-      <CenteredModal />
-      <IconButton
-        aria-label="Toggle color mode"
-        variant="ghost"
-        colorScheme="neutral"
-        size="sm"
-        rounded="$md"
-        fontSize="$lg"
-        icon={colorModeButtonIcon}
-        onClick={toggleColorMode}
-      />
-      </Flex>
-    </Container>
-  );
+    return (
+        <Container h="$full">
+            <Flex>
+                <Spacer/>
+                <CenteredModal/>
+                <IconButton
+                    aria-label="Toggle color mode"
+                    variant="ghost"
+                    colorScheme="neutral"
+                    size="sm"
+                    rounded="$md"
+                    fontSize="$lg"
+                    icon={colorModeButtonIcon}
+                    onClick={toggleColorMode}
+                />
+                <A href="/metrics">
+                    <IconButton
+                        aria-label="Toggle color mode"
+                        variant="ghost"
+                        colorScheme="neutral"
+                        size="sm"
+                        rounded="$md"
+                        fontSize="$lg"
+                        icon={IconBCH}
+                    />
+                </A>
+            </Flex>
+        </Container>
+    );
 };
 
 export default Header;
