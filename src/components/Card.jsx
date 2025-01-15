4b80d893ff17ddd93064bcff11606257b28ed715
JettScythe <jettscythe@hotmail.com>
2025-01-15 19:41:08 -0400

    update deps and links

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,20 +7,8 @@ const Card = () => {
       <div class="row">
         <div class="column">
           <div class="card">
-            <p>Social</p>
-            <Divider color={"$primary10"} />
-            <Anchor href="https://x.com" class="row" external>
-              X
-            </Anchor>
-            <Anchor href="https://memo.cash" class="row" external>
-              Memo
-            </Anchor>
-          </div>
-        </div>
-        <div class="column">
-          <div class="card">
             <p>Dev</p>
-            <Divider color={"$accent10"} />
+            <Divider color={"$accent10"}/>
             <Anchor href="https://github.com" class="row" external>
               Github
             </Anchor>
@@ -32,39 +20,21 @@ const Card = () => {
             </Anchor>
           </div>
         </div>
-        <div class="column">
-          <div class="card">
-            <p>Media</p>
-            <Divider color={"$warning10"} />
-            <Anchor href="https://jellyfin.jettscythe.xyz" class="row" external>
-              Jellyfin
-            </Anchor>
-            <Anchor href="https://youtube.com" class="row" external>
-              Youtube
-            </Anchor>
-            <Anchor href="https://seerr.jettscythe.local" class="row" external>
-              Seerr
-            </Anchor>
-          </div>
-        </div>
         <div className="column">
           <div className="card">
             <p>Admin</p>
-            <Divider color={"$danger10"} />
-            <Anchor href="http://192.168.88.228/admin" class="row" external>
-              Pi-hole (main)
-            </Anchor>
-            <Anchor href="https://192.168.88.238:8006" class="row" external>
+            <Divider color={"$danger10"}/>
+            <Anchor href="https://proxmox.jettscythe.home.arpa" class="row" external>
               Proxmox
             </Anchor>
-            <Anchor href="http://192.168.88.252" class="row" external>
+            <Anchor href="https://asus.jettscythe.home.arpa" class="row" external>
               Asus
             </Anchor>
-            <Anchor href="https://192.168.88.237" class="row" external>
+            <Anchor href="https://idrac.jettscythe.home.arpa" class="row" external>
               IDRAC
             </Anchor>
             <Anchor
-                href="https://torrents.jettscythe.local"
+                href="https://torrents.jettscythe.home.arpa"
                 class="row"
                 external
             >
@@ -77,21 +47,39 @@ const Card = () => {
             >
               grafana
             </Anchor>
-            <Anchor href="https://sonarr.jettscythe.local" class="row" external>
-              sonarr
-            </Anchor>
-            <Anchor href="https://radarr.jettscythe.local" class="row" external>
-              radarr
-            </Anchor>
-            <Anchor href="https://lidarr.jettscythe.local" class="row" external>
-              lidarr
-            </Anchor>
             <Anchor
-                href="https://prowlarr.jettscythe.local"
+                href="https://prowlarr.jettscythe.home.arpa"
                 class="row"
                 external
             >
               prowlarr
+            </Anchor>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <p>Media</p>
+            <Divider color={"$warning10"}/>
+            <Anchor href="https://jellyfin.jettscythe.xyz" class="row" external>
+              Jellyfin
+            </Anchor>
+            <Anchor href="https://youtube.com" class="row" external>
+              Youtube
+            </Anchor>
+            <Anchor href="https://seerr.jettscythe.home.arpa" class="row" external>
+              Seerr
+            </Anchor>
+            <Anchor href="https://immich.jettscythe.xyz" class="row" external>
+              Immich
+            </Anchor>
+            <Anchor href="https://sonarr.jettscythe.home.arpa" class="row" external>
+              sonarr
+            </Anchor>
+            <Anchor href="https://radarr.jettscythe.home.arpa" class="row" external>
+              radarr
+            </Anchor>
+            <Anchor href="https://lidarr.jettscythe.home.arpa" class="row" external>
+              lidarr
             </Anchor>
           </div>
         </div>
