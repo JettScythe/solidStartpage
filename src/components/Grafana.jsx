9919dc198f2d9e4835a3134c205daef12110e049
Jett Scythe <jettscythe@hotmail.com>
2025-03-10 19:04:20 -0300

    swap from embedding to sandboxed iframe

--- src/components/Grafana.jsx
+++ src/components/Grafana.jsx
@@ -1,6 +1,6 @@
 import "../css/Grafana.css"
 
 const Grafana = () => {
-    return <embed src="https://metrics.jettscythe.xyz/public-dashboards/0c3823b0103d4fa8a90d03f3f715742f" width="100%" height="100%"/>;
+    return <iframe src="https://metrics.jettscythe.xyz/public-dashboards/0c3823b0103d4fa8a90d03f3f715742f" width="100%" height="100%" sandbox={"allow-scripts allow-same-origin"}/>;
 }
 export default Grafana;
